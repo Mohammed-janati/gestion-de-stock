ff3de830d2b1afb569f4c92dda29b97bffeb0e6f
Mohammed-janati <mohammedjanati0303@gmail.com>
2025-11-05 11:35:16 +0100

    adding a load balancer

--- terraform/main/network.tf
+++ terraform/main/network.tf
@@ -16,7 +16,7 @@ resource "aws_subnet" "private_subnet" {
 }
 resource "aws_subnet" "private_subnet2" {
   vpc_id = aws_vpc.vpc.id
-  cidr_block = "10.0.2.0/24"
+  cidr_block = "10.0.3.0/24"
   availability_zone = "af-south-1b"
   map_public_ip_on_launch = false
   tags = {
@@ -26,7 +26,7 @@ resource "aws_subnet" "private_subnet2" {
 
 resource "aws_subnet" "public_subnet" {
   vpc_id = aws_vpc.vpc.id
-  cidr_block = "10.0.3.0/24"
+  cidr_block = "10.0.2.0/24"
   map_public_ip_on_launch = true
   availability_zone = "af-south-1a"
 
