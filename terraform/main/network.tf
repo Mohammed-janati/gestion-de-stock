0302c7a8e4af25f5ad1673121be0d10b5a177c67
Mohammed-janati <mohammedjanati0303@gmail.com>
2025-11-18 17:09:55 +0100

    demo

--- terraform/main/network.tf
+++ terraform/main/network.tf
@@ -8,24 +8,24 @@ resource "aws_vpc" "vpc" {
   }
 }
 
-resource "aws_subnet" "private_subnet" {
-  vpc_id = aws_vpc.vpc.id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = "af-south-1a"
-  map_public_ip_on_launch = false
-  tags = {
-    Name= "private subnet"
-  }
-}
-resource "aws_subnet" "private_subnet2" {
-  vpc_id = aws_vpc.vpc.id
-  cidr_block = "10.0.3.0/24"
-  availability_zone = "af-south-1b"
-  map_public_ip_on_launch = false
-  tags = {
-    Name= "private subnet 2"
-  }
-}
+# resource "aws_subnet" "private_subnet" {
+#   vpc_id = aws_vpc.vpc.id
+#   cidr_block = "10.0.1.0/24"
+#   availability_zone = "af-south-1a"
+#   map_public_ip_on_launch = false
+#   tags = {
+#     Name= "private subnet"
+#   }
+# }
+# resource "aws_subnet" "private_subnet2" {
+#   vpc_id = aws_vpc.vpc.id
+#   cidr_block = "10.0.3.0/24"
+#   availability_zone = "af-south-1b"
+#   map_public_ip_on_launch = false
+#   tags = {
+#     Name= "private subnet 2"
+#   }
+# }
 
 resource "aws_subnet" "public_subnet" {
   vpc_id = aws_vpc.vpc.id
@@ -70,31 +70,31 @@ resource "aws_route_table_association" "public_subnet" {
   subnet_id = aws_subnet.public_subnet.id
 }
 
-resource "aws_eip" "eip"{
- domain = "vpc"
-  tags = {
-    Name="stackapp elastic ip"
-  }
-}
+# resource "aws_eip" "eip"{
+#  domain = "vpc"
+#   tags = {
+#     Name="stackapp elastic ip"
+#   }
+# }
+#
+# resource "aws_nat_gateway" "nat_GW" {
+#   subnet_id = aws_subnet.public_subnet.id
+#   allocation_id = aws_eip.eip.id
+# }
 
-resource "aws_nat_gateway" "nat_GW" {
-  subnet_id = aws_subnet.public_subnet.id
-  allocation_id = aws_eip.eip.id
-}
+#
+# resource "aws_route_table" "privateRt" {
+#   vpc_id = aws_vpc.vpc.id
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     nat_gateway_id = aws_nat_gateway.nat_GW.id
+#   }
+#   tags = {
+#     Name="private route table"
+#   }
+# }
 
-
-resource "aws_route_table" "privateRt" {
-  vpc_id = aws_vpc.vpc.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat_GW.id
-  }
-  tags = {
-    Name="private route table"
-  }
-}
-
-resource "aws_route_table_association" "private" {
-  route_table_id = aws_route_table.privateRt.id
-  subnet_id = aws_subnet.private_subnet.id
-}
+# resource "aws_route_table_association" "private" {
+#   route_table_id = aws_route_table.privateRt.id
+#   subnet_id = aws_subnet.private_subnet.id
+# }
